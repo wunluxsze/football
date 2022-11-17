48179cae70a2900dba1be0026c28946534e59705
wunluxsze <113801343+wunluxsze@users.noreply.github.com>
2022-11-17 08:42:39 +0300

    Add files via upload

---
+++ sobitia/assets/js/style.js
@@ -0,0 +1,31 @@
+
+field.onclick = function(event){
+    let Coords = this.getBoundingClientRect();
+    let elem = {
+        top: event.clientY - Coords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - Coords.left - field.clientLeft - ball.clientWidth / 2,
+    };
+    if (elem.top < 0) elem.top = 0;
+    if (elem.left < 0) elem.left = 0;
+    if (elem.left + ball.clientWidth > field.clientWidth){
+        elem.top = field.clientWidth - ball.clientWidth;
+    }
+    if (elem.top + ball.clientHeight > field.clientHeight) {
+        elem.top = field.clientHeight - ball.clientHeight;
+      }
+    ball.style.left = elem.left + 'px';
+    ball.style.top = elem.top + 'px';
+}
+
+let horse = document.getElementById('paneHorse');
+let ishak = document.getElementById('paneIshak');
+let cat = document.getElementById('paneCat');
+function removeHorse(){
+    horse.style = `display: none;`
+};
+function removeIshak(){
+    ishak.style = `display: none;`
+};
+function removeCat(){
+    cat.style = `display: none;`
+};
